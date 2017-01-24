a9e4c2207273c1e4d478b33590a2c432be295906
Anthony Simmons <anthonysimmons@Anthonys-MacBook-Pro.local>
2017-01-24 15:59:50 -0500

    added logic for card matching

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if (cardOne === cardTwo || cardThree === cardFour) 
+	{alert ("You found a match!!");
+}
+
+else {
+	alert ("Sorry, try again");
+} 
+	
