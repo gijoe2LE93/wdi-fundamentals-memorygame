b19067069cb84c03c3f43598bfb734fb498c5979
Anthony Simmons <anthonysimmons@Anthonys-MacBook-Pro.local>
2017-02-06 12:02:53 -0500

    added JavaScript for Cards_Lesson 10

--- main.js
+++ main.js
@@ -4,12 +4,29 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo || cardThree === cardFour) 
+/*if (cardOne === cardTwo || cardThree === cardFour) 
 	{alert ("You found a match!!");
 }
 
 else {
 	alert ("Sorry, try again");
-}
+} 
+*/
 
+
+
+// Get an element with the id of game-board and set it to a variable.
+var gameboard = document.getElementById('game-board');
+
+// create a for loop that creates an HTML element for each card. 
+for (var i = 0; i < 4; i++) {
+
+// Each HTML element should be a div and have the class card
+var newCard = document.createElement('div');
+newCard.className = 'card';
+
+//Append each newly created card to the div with the ID game-board
+
+document.querySelector('#game-board').appendChild(newCard);
+}
 	
